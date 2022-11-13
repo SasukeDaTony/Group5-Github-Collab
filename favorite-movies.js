f2d0a62d304c8c30fc6e1a5526998f4373a39482
Bo Latt <bolattny@gmail.com>
2022-11-13 10:26:58 -0500

    add bo movies

--- favorite-movies.js
+++ favorite-movies.js
@@ -1,9 +1,12 @@
- const favoriteMovies  = {
-    ProjectManager: {Name:"Anthony Taylor", favMovies:["Wukanda Forever", "DeadPool", "The Bourne Identity"]},
-    MemberOne: {Name: "Bo Latt",
-   favMovies:[]},
-    MemebrTwo: {Name:"Kat Pelle",
-   favMovies:["Walk To Remember", "Lion King", "Black Panther"]},
-    MemberThree:{Name: "Rodica Chavez",
-   favMovies: []}
- }
+const favoriteMovies = {
+  ProjectManager: {
+    Name: "Anthony Taylor",
+    favMovies: ["Wukanda Forever", "DeadPool", "The Bourne Identity"],
+  },
+  MemberOne: { Name: "Bo Latt", favMovies: ["Shutter Island"] },
+  MemebrTwo: {
+    Name: "Kat Pelle",
+    favMovies: ["Walk To Remember", "Lion King", "Black Panther"],
+  },
+  MemberThree: { Name: "Rodica Chavez", favMovies: [] },
+};
