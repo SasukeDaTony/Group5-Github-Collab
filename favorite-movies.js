4b17bd0498790e326106feb6b94be73731b76fb1
SasukeDaTony <tonytaylor@pursuit.org>
2022-11-13 09:43:28 -0500

    New-Movies-ProjectManager

--- favorite-movies.js
+++ favorite-movies.js
@@ -1,5 +1,5 @@
  const favoriteMovies  = {
-    ProjectManager: {Name:"Anthony Taylor", favMovies:["Wukanda Forever", "DeadPool","Aladdin Original"]}
+    ProjectManager: {Name:"Anthony Taylor", favMovies:["Wukanda Forever", "DeadPool","Aladdin Original", "The Lion King", "The Bourne Identity"]}
     MemberOne: {Name: "Bo Latt",
    favMovies:[]}
     MemebrTwo: {Name:"Kat Pelle",
